3067c39c7ee2cb7199a9e7c15b8f988f0de29fb7
Marcin Milewicz <marcinmilewicz87@gmail.com>
2015-01-24 06:08:24 +0100

    format code

--- app/src/main/java/pl/androidland/studia/tirt/diabetichelper/ui/validators/AuthValidator.java
+++ app/src/main/java/pl/androidland/studia/tirt/diabetichelper/ui/validators/AuthValidator.java
@@ -1,4 +1,39 @@
 package pl.androidland.studia.tirt.diabetichelper.ui.validators;
 
+import android.content.Context;
+import android.widget.Toast;
+import pl.androidland.studia.tirt.diabetichelper.R;
+import pl.androidland.studia.tirt.diabetichelper.database.models.User;
+import pl.androidland.studia.tirt.diabetichelper.database.services.DatabaseService;
+import pl.androidland.studia.tirt.diabetichelper.utils.EncryptionUtils;
+
+
 public class AuthValidator {
+
+    private Context context;
+
+    public AuthValidator(Context context) {
+        this.context = context;
+    }
+
+    public boolean validatePeselId(String peselId) {
+        if (!DatabaseService.isPatientRegistered(peselId)) {
+            Toast.makeText(context, context.getString(R.string.TOAST_MESSAGE_PESEL_ALREADY_REGISTERED), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validatePassword(String peselId, String password) {
+        User user = DatabaseService.getPatientByPesel(peselId);
+        if (user == null)
+            return false;
+        String encryptedPassword = EncryptionUtils.generateMd5Hash(password);
+        String storedPassword = user.getPassword();
+        if (!storedPassword.equals(encryptedPassword)) {
+            Toast.makeText(context, context.getString(R.string.TOAST_MESSAGE_INCORRECT_PASSWORD), Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
 }
--- app/src/main/java/pl/androidland/studia/tirt/diabetichelper/ui/validators/UserValidator.java
+++ app/src/main/java/pl/androidland/studia/tirt/diabetichelper/ui/validators/UserValidator.java
@@ -1,4 +1,39 @@
 package pl.androidland.studia.tirt.diabetichelper.ui.validators;
 
+import android.widget.Toast;
+import pl.androidland.studia.tirt.diabetichelper.R;
+import pl.androidland.studia.tirt.diabetichelper.database.services.DatabaseService;
+import pl.androidland.studia.tirt.diabetichelper.ui.android.components.activities.UserActivity;
+
+import java.util.regex.Pattern;
+
+
 public class UserValidator {
+    private static final Pattern DECIMAL_NUMBER_PATERN = Pattern.compile("\\d+\\.\\d+");
+    private UserActivity callback;
+
+    public UserValidator(UserActivity callback) {
+        this.callback = callback;
+    }
+
+    public boolean validatePatient(String pesel) {
+        if (!DatabaseService.isPatientRegistered(pesel)) {
+            Toast.makeText(callback, callback.getString(R.string.TOAST_MESSAGE_PATIENT_DOES_NOT_EXIST),
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateMeasurement(String measurement) {
+        if (!DECIMAL_NUMBER_PATERN.matcher(measurement).matches()) {
+            Toast.makeText(callback,
+                    callback.getString(R.string.TOAST_MESSAGE_UNSUPPORTED_MEASUREMENT_FORMAT),
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+
+    }
 }
